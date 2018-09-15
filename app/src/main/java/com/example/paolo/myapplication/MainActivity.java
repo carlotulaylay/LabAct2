fc300629112579365e47061793668a4cba676a8a
carlotulaylay <kristiantulaylay@gmail.com>
2018-09-15 17:32:06 +0800

    ano lalagay dito

--- app/src/main/java/com/example/paolo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/paolo/myapplication/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.paolo.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linear);
+        setContentView(R.layout.main);
+
+        Button constraint = (Button) findViewById(R.id.constraint);
+        constraint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent moveSecond = new Intent(getApplicationContext(), ConstraintactActivity.class);
+                startActivity(moveSecond);
+            }
+        });
+
+
+        Button linear = (Button) findViewById(R.id.linear);
+        linear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent moveSecond = new Intent(getApplicationContext(), LinearactActivity.class);
+                startActivity(moveSecond);
+            }
+        });
+
+
+        Button activity_main = (Button) findViewById(R.id.relative);
+        activity_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent moveSecond = new Intent(getApplicationContext(), RelativeactActivity.class);
+                startActivity(moveSecond);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/paolo/myapplication/RelativeactActivity.java
@@ -0,0 +1,13 @@
+package com.example.paolo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RelativeactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
